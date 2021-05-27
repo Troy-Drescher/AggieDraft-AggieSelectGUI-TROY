f636778bc8d0ed717b4fc6ada1a16f1c89b1beca
KingElrond <66854137+KingElrond@users.noreply.github.com>
2021-05-26 21:25:24 -0700

    now stupid branch is even with Jonathans stuff

--- Abilities.java
+++ Abilities.java
@@ -8,14 +8,6 @@ public class Abilities{
 	private AbilityButton button;
 	private String tooltip;
 	
-	public Abilities(String name, int type, int value, int stat, String tooltip) { // buff or debuff
-		this.name = name;
-		this.type = type;
-		abilityValue = value;
-		this.stat = stat;
-		this.tooltip = tooltip;
-	}
-	
 	public Abilities(String name, int type, String tooltip) { // attack
 		this.name = name;
 		this.type = type;
@@ -26,6 +18,7 @@ public class Abilities{
 		this.name = name;
 		this.type = type;
 		this.effect = effect;
+		this.stat = effect;
 		this.tooltip = tooltip;
 	}
 	
@@ -36,12 +29,23 @@ public class Abilities{
 	public int getStat() {
 		return stat;
 	}
+	public String getStatName() {
+		switch(stat) {
+		case 0:
+			return "damage";
+		case 1: 
+			return "speed";
+		case 2:
+			return "defense";
+		default: 
+			return "health";
+		}
+	}
+	
 	public String getName() {
 		return name;
 	}
-  public String getDesc() {
-    return tooltip;
-  }
+
 	public int getValue() {
 		return abilityValue;
 	}
--- Main.java
+++ Main.java
@@ -8,64 +8,33 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.util.Scanner;
-import javax.swing.Box;
-import java.awt.GridLayout;
+
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
-
-import java.awt.BorderLayout;
-import java.awt.Container;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
-import java.awt.FlowLayout;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
-import javax.swing.JTextArea;
 import javax.swing.JToggleButton;
-import javax.swing.JButton;
-import javax.swing.JOptionPane;
 import javax.swing.Timer;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.table.DefaultTableModel;
 
 public class Main extends JPanel implements ActionListener{ // Add panel on side that shows each creature stats
 
-	static JFrame frame,tableFrame,selectFrame,AggieFrame;
+	static JFrame frame,tableFrame;
 	static final int width = 410, height = 410;
-	private BufferedImage icon;
-  private JLabel agIMG;
-  private JTextArea larea,Rarea;
-  static ImageIcon ag1Icon = new ImageIcon("rayquaza.png");
-  static ImageIcon ag2Icon = new ImageIcon("pika.png");
-/*
-  private ImageIcon SportBlack = new ImageIcon("SportBlack.jpeg");
-  private ImageIcon SportBlue = new ImageIcon("SportBlue.jpeg");
-  private ImageIcon SportWhite = new ImageIcon("SportWhite.jpeg"); 
-  */
+	private BufferedImage icon; 
 	static Aggie p1,p2;
-  static Aggie[] AggieLists;
-	static JToggleButton toggle, p1sel,p2sel;
-  private int cAGG=0;
-  static JButton nextAG,prevAG,agselStart;
+	static JToggleButton toggle;
 	private JTable table = new JTable();
 	private JScrollPane container;
 	static DefaultTableModel model = new DefaultTableModel();
 	private Graphics2D ga;
 	static Timer timer;
 	static JTextField display = new JTextField();
-  static String agselAB1,agselAB2,agselAB3,agselAB4;
-  static JTextField agSelName,agselStats;
-  static String agselAB1t,agselAB2t,agselAB3t,agselAB4t;
 	static String str;
 	static int end;
 	private static Scanner kb = new Scanner(System.in);
@@ -73,6 +42,9 @@ public class Main extends JPanel implements ActionListener{ // Add panel on side
 	public Main(Aggie player1, Aggie player2) {
 		p1 = player1;
 		p2 = player2;
+		
+		p1.setAnimations(-1, 1);
+		p2.setAnimations(1, -1);
 		
 		setSize(width,height);  // creating panel to place on frame
 		setLayout(null);
@@ -88,8 +60,6 @@ public class Main extends JPanel implements ActionListener{ // Add panel on side
 			System.out.println("Failed to load file");
 		}
 		
-
-
 		
 		toggle = new JToggleButton(); // adds toggle button
 		toggle.setBounds(width-32,-5,icon.getWidth()-5,icon.getHeight()-5);
@@ -145,105 +115,6 @@ public class Main extends JPanel implements ActionListener{ // Add panel on side
 		p2.render(ga);
 		ga.drawImage(icon, width - 25, 2, icon.getWidth()/2, icon.getHeight()/2, null);
 	}
-  public Main(String aggieselectString)
-  {
-    CompoundBorder border = new CompoundBorder(
-        new EmptyBorder(6, 6, 6, 6),     // 6 pixels on top, left, bottom, right
-        new LineBorder(Color.black, 1)); // outside, 1 
-  JPanel agSbuttons = new JPanel();
-  agSbuttons.setLayout(new GridLayout(3, 1, 10, 10));
-  JPanel agS2buttons = new JPanel();
-  agS2buttons.setLayout(new GridLayout(3, 1, 10, 10));
-  JPanel agS3buttons = new JPanel();
-  agS3buttons.setLayout(new GridLayout(3, 1, 10, 10));
-  nextAG = new JButton("Next");
-  prevAG = new JButton("Prev");
-  agselStart = new JButton("Start");
-  nextAG.addActionListener(this);
-  prevAG.addActionListener(this);
-  agselStart.addActionListener(this);
-agS2buttons.add(nextAG);
-agS3buttons.add(agselStart);
-agSbuttons.add(prevAG);
-FlowLayout fl = new FlowLayout();
-fl.setAlignment(FlowLayout.CENTER);
-JPanel aggieImageSelection = new JPanel();
-aggieImageSelection.setLayout(fl);
-
-    agIMG = new JLabel();
-    agIMG.setIcon(ag1Icon);
-    agIMG.setOpaque(true);
-    
-//did stuff
-JPanel agimgseltext = new JPanel();
-  agSelName = new JTextField("Medinasaur");
-  agSelName.setEditable(false);
-  agimgseltext.add(agSelName);
-
-JPanel agselLeft = new JPanel();
-JPanel agselright = new JPanel();
-agselAB1 = AggieLists[cAGG].getAbilities()[0].getName();
-agselAB2 = AggieLists[cAGG].getAbilities()[1].getName();
-agselAB3 = AggieLists[cAGG].getAbilities()[2].getName();
-agselAB4 = AggieLists[cAGG].getAbilities()[3].getName();
-agselAB1t = AggieLists[cAGG].getAbilities()[0].getDesc();
-agselAB2t = AggieLists[cAGG].getAbilities()[1].getDesc();
-agselAB3t = AggieLists[cAGG].getAbilities()[2].getDesc();
-agselAB4t = AggieLists[cAGG].getAbilities()[3].getDesc();
-larea = new JTextArea();
-larea.setText(agselAB1 + "\n" + agselAB1t + "\n\n" + agselAB2 + "\n" + agselAB2t);
-Rarea = new JTextArea();
-Rarea.setText(agselAB3 + "\n" + agselAB3t + "\n\n" + agselAB4 + "\n" + agselAB4t);
-larea.setColumns(26);
-Rarea.setColumns(26);
-larea.setLineWrap(true);
-Rarea.setLineWrap(true);
-aggieImageSelection.add(larea);
-aggieImageSelection.add(agIMG);
-aggieImageSelection.add(Rarea);
-
-Box hbox = Box.createHorizontalBox();
-    hbox.add(Box.createHorizontalStrut(10));
-    hbox.add(agSbuttons);
-    hbox.add(Box.createHorizontalStrut(10));
-    hbox.add(agS3buttons);
-    hbox.add(Box.createHorizontalStrut(10));
-    hbox.add(agS2buttons);
-    hbox.add(Box.createHorizontalStrut(10));
-Box vbox = Box.createVerticalBox();
-    vbox.add(Box.createVerticalStrut(5));
-    vbox.add(agimgseltext);
-    vbox.add(Box.createVerticalStrut(5));
-    vbox.add(aggieImageSelection);
-Box h1box = Box.createHorizontalBox();
-h1box.add(Box.createHorizontalStrut(10));
-h1box.add(agselLeft);
-h1box.add(Box.createHorizontalStrut(10));
-Box h2box = Box.createHorizontalBox();
-h2box.add(Box.createHorizontalStrut(10));
-h2box.add(agselright);
-h2box.add(Box.createHorizontalStrut(10));
-
-
-//I DID IT
-Container c = new Container();
-    c.setLayout(new BorderLayout(10,5));
-    c.add(hbox, BorderLayout.NORTH);
-    c.add(vbox, BorderLayout.CENTER);
-
-    selectFrame = new JFrame();
-		selectFrame.setSize(width+200,height);
-		selectFrame.setLocationRelativeTo(null);
-    selectFrame.setBackground(Color.BLACK);
-    selectFrame.add(c);
-		selectFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    selectFrame.setResizable(true);
-    selectFrame.setVisible(true);
-
-
-
-
-  }
 	
 	public static void main(String[] args){
 		try {
@@ -253,35 +124,49 @@ Container c = new Container();
 			}
 		}
 		catch(Exception e) {}
-		Aggie a1 = new Medinasaur(new Sprite("rayquaza.png"),100,"Medinasuar",50,50,50,125,ag1Icon);
-		Aggie a2 = new Penisaur(new Sprite("pika.png"),100,"Penisaur",75,80,50,70,ag2Icon);
-    // params(Sprite, dimX, dimY, size, name, damage, speed, defense, health)
-		Aggie[] aggieList = {a1,a2};
-    AggieLists = aggieList;
+		
+		// params(Sprite, name, damage, speed, defense, health)
+		Aggie [] aggieList = {
+				new Medinasaur(new Sprite("Medinasaur1.png"),"Medinasaur",50,50,50,125),
+				new Penisaur(new Sprite("Penisaur1.png"),"Penisaur",75,80,50,70),
+				new Finality(new Sprite("Finality1.png"),"Finality",125, 75,15,60),
+				new Charchimp(new Sprite("Charchimp1.png"),"Charchimp",100, 40, 60, 75),
+				new Iceguin(new Sprite("Iceguin1.png"),"Iceguin",125, 40, 35, 75),
+				new Pegasus(new Sprite("Pegasus1.png"),"Pegasus",70,50,70,80)
+		};
+		
 		// params(Sprite, dimX, dimY, size, name, damage, speed, defense, health)
 		System.out.println("Select the first Aggie\n");
 		for(int i = 0; i < aggieList.length;i++) {
 			System.out.println(i+1+ ") " + aggieList[i].getName());
 		}
 		Aggie player1 = aggieList[kb.nextInt()-1];
+		for(Aggie i : aggieList) {
+			if(player1 == i)
+				i.setSprite(i.getName() + "1.png" );
+		}
+		
 		
 		System.out.println("Select the second Aggie");
 		for(int i = 0; i < aggieList.length;i++) {
 			System.out.println(i+1 + ") " + aggieList[i].getName());
 		}
 		Aggie player2 = aggieList[kb.nextInt()-1];
+		for(Aggie i : aggieList) {
+			if(player2 == i)
+				i.setSprite(i.getName() + "2.png" );
+		}
 		
 		frame = new JFrame();
 		frame.setSize(width,height);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(true);
-    new Main("aggieString");
+		frame.setResizable(false);
 		
 		player1.setXpos(250);
 		player1.setYpos(20);
 		
-		player2.setXpos(60);
+		player2.setXpos(50);
 		player2.setYpos(160);
 		
 		frame.add(new Main(player1, player2));
@@ -320,13 +205,6 @@ Container c = new Container();
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-    if(nextAG.isSelected())
-    {
-      cAGG+=1;
-      agIMG.setIcon(AggieLists[cAGG].getselIcon());
-      JOptionPane.showMessageDialog(Main.this, "Image preview Updated");
-      
-    }
 		if(end <= str.length()) {
 			display.setText(str.substring(0, end));
 			end++;
@@ -340,7 +218,6 @@ Container c = new Container();
 			} catch (InterruptedException e1) {}
 			timer.stop();
 		}
-
 	}
 	
 	public static void updateTable() {
