65706ba9ae9a865826b72fb7c28c203636dca1f1
Troy Drescher <>
2021-05-27 01:40:05 +0000

    fixed

--- Main.java
+++ Main.java
@@ -18,6 +18,7 @@ import java.awt.Container;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
+import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -29,6 +30,7 @@ import javax.swing.JTextField;
 import javax.swing.JTextArea;
 import javax.swing.JToggleButton;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.Timer;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -42,6 +44,7 @@ public class Main extends JPanel implements ActionListener{ // Add panel on side
 	static final int width = 410, height = 410;
 	private BufferedImage icon;
   private JLabel agIMG;
+  private JTextArea larea,Rarea;
   static ImageIcon ag1Icon = new ImageIcon("rayquaza.png");
   static ImageIcon ag2Icon = new ImageIcon("pika.png");
 /*
@@ -52,6 +55,7 @@ public class Main extends JPanel implements ActionListener{ // Add panel on side
 	static Aggie p1,p2;
   static Aggie[] AggieLists;
 	static JToggleButton toggle, p1sel,p2sel;
+  private int cAGG=0;
   static JButton nextAG,prevAG,agselStart;
 	private JTable table = new JTable();
 	private JScrollPane container;
@@ -143,6 +147,9 @@ public class Main extends JPanel implements ActionListener{ // Add panel on side
 	}
   public Main(String aggieselectString)
   {
+    CompoundBorder border = new CompoundBorder(
+        new EmptyBorder(6, 6, 6, 6),     // 6 pixels on top, left, bottom, right
+        new LineBorder(Color.black, 1)); // outside, 1 
   JPanel agSbuttons = new JPanel();
   agSbuttons.setLayout(new GridLayout(3, 1, 10, 10));
   JPanel agS2buttons = new JPanel();
@@ -158,35 +165,42 @@ public class Main extends JPanel implements ActionListener{ // Add panel on side
 agS2buttons.add(nextAG);
 agS3buttons.add(agselStart);
 agSbuttons.add(prevAG);
-
+FlowLayout fl = new FlowLayout();
+fl.setAlignment(FlowLayout.CENTER);
 JPanel aggieImageSelection = new JPanel();
+aggieImageSelection.setLayout(fl);
+
     agIMG = new JLabel();
     agIMG.setIcon(ag1Icon);
     agIMG.setOpaque(true);
-    aggieImageSelection.add(agIMG);
-
+    
+//did stuff
 JPanel agimgseltext = new JPanel();
-agimseltext.setLayout(new GridLayout(2,3,10,10));
   agSelName = new JTextField("Medinasaur");
   agSelName.setEditable(false);
   agimgseltext.add(agSelName);
 
 JPanel agselLeft = new JPanel();
 JPanel agselright = new JPanel();
-agselAB1 = AggieLists[0].getAbilities()[0].getName();
-agselAB2 = AggieLists[0].getAbilities()[1].getName();
-agselAB3 = AggieLists[0].getAbilities()[2].getName();
-agselAB4 = AggieLists[0].getAbilities()[3].getName();
-agselAB1t = AggieLists[0].getAbilities()[0].getDesc();
-agselAB2t = AggieLists[0].getAbilities()[1].getDesc();
-agselAB3t = AggieLists[0].getAbilities()[2].getDesc();
-agselAB4t = AggieLists[0].getAbilities()[3].getDesc();
-JTextArea larea = new JTextArea();
+agselAB1 = AggieLists[cAGG].getAbilities()[0].getName();
+agselAB2 = AggieLists[cAGG].getAbilities()[1].getName();
+agselAB3 = AggieLists[cAGG].getAbilities()[2].getName();
+agselAB4 = AggieLists[cAGG].getAbilities()[3].getName();
+agselAB1t = AggieLists[cAGG].getAbilities()[0].getDesc();
+agselAB2t = AggieLists[cAGG].getAbilities()[1].getDesc();
+agselAB3t = AggieLists[cAGG].getAbilities()[2].getDesc();
+agselAB4t = AggieLists[cAGG].getAbilities()[3].getDesc();
+larea = new JTextArea();
 larea.setText(agselAB1 + "\n" + agselAB1t + "\n\n" + agselAB2 + "\n" + agselAB2t);
-JTextArea Rarea = new JTextArea();
+Rarea = new JTextArea();
 Rarea.setText(agselAB3 + "\n" + agselAB3t + "\n\n" + agselAB4 + "\n" + agselAB4t);
-agselLeft.add(larea);
-agselright.add(Rarea);
+larea.setColumns(26);
+Rarea.setColumns(26);
+larea.setLineWrap(true);
+Rarea.setLineWrap(true);
+aggieImageSelection.add(larea);
+aggieImageSelection.add(agIMG);
+aggieImageSelection.add(Rarea);
 
 Box hbox = Box.createHorizontalBox();
     hbox.add(Box.createHorizontalStrut(10));
@@ -213,14 +227,12 @@ h2box.add(Box.createHorizontalStrut(10));
 
 //I DID IT
 Container c = new Container();
-    c.setLayout(new BorderLayout(10,10));
+    c.setLayout(new BorderLayout(10,5));
     c.add(hbox, BorderLayout.NORTH);
-    c.add(h1box, BorderLayout.WEST);
     c.add(vbox, BorderLayout.CENTER);
-    c.add(h2box, BorderLayout.EAST);
 
     selectFrame = new JFrame();
-		selectFrame.setSize(width,height);
+		selectFrame.setSize(width+200,height);
 		selectFrame.setLocationRelativeTo(null);
     selectFrame.setBackground(Color.BLACK);
     selectFrame.add(c);
@@ -264,7 +276,6 @@ Container c = new Container();
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(true);
-
     new Main("aggieString");
 		
 		player1.setXpos(250);
@@ -309,6 +320,13 @@ Container c = new Container();
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+    if(nextAG.isSelected())
+    {
+      cAGG+=1;
+      agIMG.setIcon(AggieLists[cAGG].getselIcon());
+      JOptionPane.showMessageDialog(Main.this, "Image preview Updated");
+      
+    }
 		if(end <= str.length()) {
 			display.setText(str.substring(0, end));
 			end++;
@@ -322,10 +340,6 @@ Container c = new Container();
 			} catch (InterruptedException e1) {}
 			timer.stop();
 		}
-    if(nextAG.isSelected())
-    {
-
-    }
 
 	}
 	
